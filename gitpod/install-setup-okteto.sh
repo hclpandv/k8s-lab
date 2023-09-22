40a200386a4914232f0f97442a0e25a54a1d9df6
Vikas Pandey <vikas110@gmail.com>
2023-09-22 02:52:16 +0000

    Updated gitpod prebuild

---
+++ gitpod/install-setup-okteto.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# install okteto
+echo "Setting-up okteto"
+OLD_DIR="$PWD"
+TMP_DIR="$(mktemp -d)"
+cd "${TMP_DIR}" || exit 1
+
+curl https://get.okteto.com -sSfL | sh
+
+# Use PAT from settings
+okteto context use https://cloud.okteto.com --token $OKTETO_PAT
+
+# Setup kubectl
+echo "Setting-up kubectl"
+OLD_DIR="$PWD"
+TMP_DIR="$(mktemp -d)"
+cd "${TMP_DIR}" || exit 1
+
+# Get the Stable version of k8s
+KUBE_REL=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+
+curl -LO "https://dl.k8s.io/release/${KUBE_REL}/bin/linux/amd64/kubectl"
+KUBE_SHA=$(curl -sL "https://dl.k8s.io/release/${KUBE_REL}/bin/linux/amd64/kubectl.sha256")
+
+echo "${KUBE_SHA} kubectl" >kubectl.sha256
+
+if sha256sum -c kubectl.sha256; then
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/kubectl
+fi
+
+cd "${OLD_DIR}" || exit 1
+rm -rf "${TMP_DIR}"
+
+
+# tell kubectl to use okteto kubeconfig
+okteto kubeconfig
+
+# validate installation success
+okteto version && kubectl version
